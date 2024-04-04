78eda01f29269370fe44d40a027104e8d1a5fdd6
Alexis <alexisct2212@gmail.com>
2024-04-03 22:02:15 -0400

    part 1 of 3 Done

--- scripts/validation.js
+++ scripts/validation.js
@@ -15,17 +15,35 @@ function hideInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
 function checkInputValidity(formEl, inputEl, options) {
   if (!inputEl.validity.valid) {
     showInputError(formEl, inputEl, options);
+    return;
+  }
+  hideInputError(formEl, inputEl, options);
+}
+
+function ToggleButtonState(inputEls, submitButton, inactiveButtonClass) {
+  let foundInvalid = false;
+  inputEls.forEach((inputEl) => {
+    if (!inputEl.validity.valid) {
+      foundInvalid = true;
+    }
+  });
+  if (foundInvalid) {
+    submitButton.classList.add(inactiveButtonClass);
+    submitButton.disabled = true;
   } else {
-    hideInputError(formEl, inputEl, options);
+    submitButton.classList.remove(inactiveButtonClass);
+    submitButton.disabled = false;
   }
 }
 
 function setEventListener(formEl, options) {
   const { inputSelector } = options;
   const inputEls = [...formEl.querySelectorAll(inputSelector)];
+  const submitButton = formEl.querySelector(".modal__button");
   inputEls.forEach((inputEl) => {
     inputEl.addEventListener("input", (e) => {
       checkInputValidity(formEl, inputEl, options);
+      ToggleButtonState(inputEls, submitButton, options);
     });
   });
 }
